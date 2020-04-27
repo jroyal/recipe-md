559e574b871d27ae31ffe6cfc500e64eee0d9356
James Royal <jhr.atx@gmail.com>
2020-04-26 20:00:13 -0500

    Add toggle and render of markdown

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { parseRecipe } from "../lib/recipe";
 import { generateMarkdown } from "../lib/markdown";
+import MarkdownIt from "markdown-it";
 
 export const index = (req: Request, res: Response) => {
   res.render("index", {
@@ -12,7 +13,13 @@ export const parseRecipeHandler = async (req: Request, res: Response) => {
   const url = req.query.recipe_url;
   const recipe = await parseRecipe(url as string);
   const markdown = await generateMarkdown(recipe);
+
+  let md = new MarkdownIt({
+    html: true,
+  });
+  let markdownHTML = md.render(markdown);
   res.render("markdown", {
     markdown: markdown,
+    markdownHTML: markdownHTML,
   });
 };
